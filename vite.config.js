f76368e74e1ef7c4cfe1b43cd7d63f95a71de26e
daridev <darideveloper@gmail.com>
2024-09-06 10:36:26 -0600

    fix (settings): update base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/will-ryan-airport-transfers/',
+  base: '/andrea-scott-airport-transfers/',
   build: {
     emptyOutDir: false,
   },
