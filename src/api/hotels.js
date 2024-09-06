9f8b481e00b103e71af2dc95f68d989b093fa69e
daridev <darideveloper@gmail.com>
2024-09-06 12:44:19 -0600

    feat (data/hotels): add real hotels

--- src/api/hotels.js
+++ src/api/hotels.js
@@ -3,13 +3,8 @@ export async function getHotels() {
   // Format data
   const data = [
     {
-      value: "sample hotel 1",
-      label: "sample hotel 1",
-      price: 0
-    },
-    {
-      value: "sample hotel 2",
-      label: "sample hotel 2",
+      value: "Hyatt Zilara Cancun",
+      label: "Hyatt Zilara Cancun",
       price: 0
     },
     {
